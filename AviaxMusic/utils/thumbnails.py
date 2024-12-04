94ba0cea5ae652e66dce2d009400a6a8377d82c7
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-04 17:03:16 +0530

    Update thumbnails.py

--- AviaxMusic/utils/thumbnails.py
+++ AviaxMusic/utils/thumbnails.py
@@ -234,8 +234,8 @@ async def gen_thumb(videoid: str):
         draw_text_with_shadow(background, draw, (1080, 400), duration, arial, (255, 255, 255))
         
         play_icons = Image.open("AviaxMusic/assets/play_icons.png")
-        play_icons = play_icons.resize((580, 150))
-        background.paste(play_icons, (text_x_position, 450), play_icons)
+        play_icons = play_icons.resize((580, 170))
+        background.paste(play_icons, (text_x_position, 480), play_icons)
 
         os.remove(f"cache/thumb{videoid}.png")
 
