da266fed692e34f05dc7d2cb267d6f227ff61b2f
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-07 06:54:14 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -5,16 +5,16 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls, StreamType
+from pytgcalls import py-tgcalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
     TelegramServerError,
 )
-from pytgcalls.types import Update
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
-from pytgcalls.types.stream import StreamAudioEnded
+from py-tgcalls.types import Update
+from py-tgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from py-tgcalls.types.input_stream.quality import HighQualityAudio, MediumQualityVideo
+from py-tgcalls.types.stream import StreamAudioEnded
 
 import config
 from AviaxMusic import LOGGER, YouTube, app
