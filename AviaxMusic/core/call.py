87a6520de8450fbf580ed5028752aec2b6a875cb
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2024-12-07 07:09:27 +0530

    Update call.py

--- AviaxMusic/core/call.py
+++ AviaxMusic/core/call.py
@@ -5,7 +5,7 @@ from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import pytgcalls, StreamType
+from pytgcalls import pyTgcalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
